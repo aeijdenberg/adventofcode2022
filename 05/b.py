8347a78f6432ce694235bb87fb21be0a4a72e16f
Adam Eijdenberg <adam@continusec.com>
2022-12-18 00:11:50 +0000

    Day 5

--- 05/b.py
+++ 05/b.py
@@ -1,14 +1,18 @@
-cur = []
-all = []
-for line in open('input.txt'):
-    line = line.strip()
-    if len(line):
-        cur.append(int(line))
-    else:
-        all.append(cur)
-        cur = []
-if len(cur):
-    all.append(cur)
-sums = [sum(x) for x in all]
-sums.sort(reverse=True)
-print(sum(sums[:3]))
+lines = [x for x in open('input.txt')]
+firstBlank = lines.index('\n')
+cols = int(lines[firstBlank - 1].strip().split(' ')[-1])
+stacks = [[] for x in range(cols)]
+for line in lines[firstBlank - 2::-1]:
+    for i in range(cols):
+        ps = 1+i*4
+        if ps <= len(line) and line[ps] != ' ':
+            stacks[i].append(line[ps])
+for line in lines[firstBlank+1:]:
+    mv, fromX, toY = [int(x) for x in line.strip().split(' ') if x.isnumeric()]
+    tmpS = []
+    for i in range(mv):
+        tmpS.append(stacks[fromX - 1].pop())
+    for i in range(mv):
+        stacks[toY - 1].append(tmpS.pop())
+
+print(''.join(x.pop() for x in stacks))
