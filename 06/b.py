48162c5092ba0d2138b840d9f811d888b9b4e50d
Adam Eijdenberg <adam@continusec.com>
2022-12-18 00:17:41 +0000

    Part 2

--- 06/b.py
+++ 06/b.py
@@ -1,14 +1,5 @@
-cur = []
-all = []
-for line in open('input.txt'):
-    line = line.strip()
-    if len(line):
-        cur.append(int(line))
-    else:
-        all.append(cur)
-        cur = []
-if len(cur):
-    all.append(cur)
-sums = [sum(x) for x in all]
-sums.sort(reverse=True)
-print(sum(sums[:3]))
+line = open('input.txt').read().strip()
+for i in range(14, len(line)):
+    if len(set(line[i-14:i])) == 14:
+        print(i)
+        break
